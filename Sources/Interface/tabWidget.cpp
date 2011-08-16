2b610a586846678d2894de4081f8e804ca86dbe7
Erèbe <nemesia.lilith@gmail.co>
2011-08-16 03:45:06 +0200

    Développement de l'interface

---
+++ Sources/Interface/tabWidget.cpp
@@ -0,0 +1,39 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  tabWidget.cpp
+ *
+ *    Description:  Tabula Rasa : Implementation d'un onglet
+ *
+ *        Version:  1.0
+ *        Created:  16/08/2011 03:43:02
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Erebe (), erebe@erebe.eu
+ *        Company:  Erebe corporation
+ *
+ * =====================================================================================
+ */
+#include "tabWidget.hpp"
+#include "algorithmeScene.hpp"
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QHBoxLayout>
+
+TabWidget::TabWidget()
+{
+     scene_ = new AlgorithmeScene( 0, 0, 2000, 2000, this );
+     scene_->setBackgroundBrush( Qt::white );
+
+     vue_ = new QGraphicsView( this );
+     vue_->setResizeAnchor( QGraphicsView::NoAnchor );
+     vue_->setTransformationAnchor( QGraphicsView::NoAnchor );
+     vue_->setRenderHint( QPainter::Antialiasing );
+     vue_->setScene( scene_ );
+
+     layout_ = new QHBoxLayout( this );
+     layout_->addWidget( vue_ );
+     layout_->setMargin( 0 );
+
+}
