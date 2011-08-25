cff4af6e570a52dd648021c120232834a70ef05c
Erèbe <erebe@erebe.eu>
2011-08-25 02:16:00 +0200

    Suppression de format.sh 	deleted:    format.sh

--- format.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# != 1 ]; then
-	echo Il faut donner le chemin où formatter les fichier
-fi
-
-find $1 -name *.hpp -exec astyle '{}' \;
-find $1 -name *.cpp -exec astyle '{}' \;
-find $1 -name *.orig -exec rm '{}' \;
