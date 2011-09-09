0889e015622d7f201454c6e2b59eaa63844f4580
Erèbe <nemesia.lilith@gmail.co>
2011-09-09 18:29:35 +0200

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-cd Sources
-qmake
-make && cp tabula_rasa ../
-
 NORMAL="\\033[39;49;00m"
 ROUGE="\\033[1;31m"
 VERT="\\033[1;32m"
+
+QMAKE=`which qmake-qt4`
+
+if [ -z "${QMAKE}" ]
+then
+    QMAKE=`which qmake`
+fi
+
+cd Sources
+${QMAKE} || exit 0
+make || exit 0
+cp tabula_rasa ../
+
 
 echo -e "\n\n${ROUGE}Vous pouvez lancer l'application en tapant la commande ${VERT}./tabula_rasa"
 echo -e "${ROUGE}Installer l'application dans ${VERT}/usr/bin ${ROUGE}? [o/n] ${NORMAL}" 
