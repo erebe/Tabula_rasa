c45f55ecd3fe85fd795fe7266cc638f08026649a
Erèbe <nemesia.lilith@gmail.co>
2011-10-09 19:53:18 +0200

    Modification du sudo make install

--- build.sh
+++ build.sh
@@ -19,15 +19,7 @@ cp tabula_rasa ../
 
 echo -e "\n\n${ROUGE}Vous pouvez lancer l'application en tapant la commande ${VERT}./tabula_rasa"
 
-echo -e "${ROUGE}Voulez-vous installer l'application dans ${VERT}/usr/bin ${ROUGE}? [o/n] ${NORMAL}" 
-
-read reponse
-
-if [ "${reponse}" == "o" ]
-then
-    sudo make install
-    echo -e "${ROUGE}Terminé ! Tapez directement ${VERT}tabula_rasa ${ROUGE}dans votre terminal pour lancer l'application${NORMAL}"
-    echo ''
-fi
+echo -e "${ROUGE}Vous pouvez installer l'application dans ${VERT}/usr/bin${ROUGE} en tapant "
+echo -e "${VERT}cd Sources; sudo make install${NORMAL}" 
 
 
