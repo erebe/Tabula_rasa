2cdb974a81173269388bb1080b57f57707e12042
Erèbe <romain.gerard@erebe.eu>
2016-11-12 14:55:59 +0100

    Main.cpp: Setting locale encoding is not needed anymore

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -27,18 +27,8 @@ int main( int argc, char* argv[] )
 {/*{{{*/
      QApplication a( argc, argv );
 
-     // FIXME is this necessary in Qt 5.5 ?
-     //QTextCodec::setCodecForCStrings( QTextCodec::codecForName( "UTF-8" ) );
-     //QTextCodec::setCodecForTr( QTextCodec::codecForName( "UTF-8" ) );
-
-     QString locale = QLocale::system().name().section( '_', 0, 0 );
-     QTranslator translator;
-     translator.load( QString( "qt_" ) + locale, QLibraryInfo::location( QLibraryInfo::TranslationsPath ) );
-     a.installTranslator( &translator );
-
-
-
      MainWindow w;
      w.showMaximized();
      return a.exec();
+
 }/*}}}*/
